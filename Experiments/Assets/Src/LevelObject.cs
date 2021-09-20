2a3f454fbd6fc23219c3792dbf06c21980ef19fa
Mario <mizar2502@aol.com>
2021-09-20 13:12:11 +0200

    Remove level manager, add static level

--- Experiments/Assets/Src/LevelObject.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class LevelObject
-{
-    public Vector3Int Position;
-    public string PrefabName;
-}
--- Experiments/Assets/Src/Scriptable Objects/LevelManager.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Level Manager", menuName = "Level/Level Manager")]
-public class LevelManager : ScriptableObject
-{
-    public Level[] Levels;
-}
