30262956d4c9cfd3f955c05a033d1cb2f1e4a4bc
Mario <mizar2502@aol.com>
2021-09-25 14:35:08 +0200

    Add scriptable object variables

--- Experiments/Assets/Src/MapManager.cs
+++ Experiments/Assets/Src/MapManager.cs
@@ -14,17 +14,29 @@ public class MapManager : MonoBehaviour
     [SerializeField]
     private ObjectType _boxType;
     [SerializeField]
+    private ObjectType _targetType;
+    [SerializeField]
     private List<TileData> _tileData;
+
+    [SerializeField]
+    private IntegerVariable _targets;
+    [SerializeField]
+    private IntegerVariable _correctBoxes;
 
     private Dictionary<TileBase, TileData> _tileDataLookup;
 
     private void Awake()
     {
+        _targets.Value = 0;
+        _correctBoxes.Value = 0;
+
         _tileDataLookup = new Dictionary<TileBase, TileData>();
         foreach (var data in _tileData)
         {
             _tileDataLookup.Add(data.Tile, data);
         }
+
+        CountAllTargets();
     }
 
     public TileData GetTileData(TileBase tile)
@@ -65,5 +77,20 @@ public class MapManager : MonoBehaviour
 
         _blockingObjects.SetTile(fromCell, null);
         _blockingObjects.SetTile(toCell, tile);
+    }
+
+    private void CountAllTargets()
+    {
+        TileData data;
+        Vector3Int cell;
+        foreach (var pos in _passableObjects.cellBounds.allPositionsWithin)
+        {
+            cell = new Vector3Int(pos.x, pos.y, pos.z);
+            data = GetTileData(_passableObjects.GetTile(cell));
+            if (data != null && data.Type == _targetType)
+            {
+                ++_targets.Value;
+            }
+        }
     }
 }
---
+++ Experiments/Assets/Src/Scriptable Objects/IntegerVariable.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Integer", menuName = "Scriptable Objects/Integer Variable")]
+public class IntegerVariable : ScriptableObject
+{
+    public int Value;
+}
